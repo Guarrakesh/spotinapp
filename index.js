70cacb156a380478d50e92c85922b0ef0daa2af4
mrshlaw <m.varrella@icloud.com>
2018-07-24 18:57:42 +0200

    restyle news card and event card

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import { AppRegistry } from 'react-native';
 import React from 'react';
-import App from './src';
+// import App from './src';
+import NewsList from './src/components/NewsComponents/NewsList';
+import NewsCard from './src/components/NewsComponents/NewsCard';
+import NewsDescription from './src/components/NewsComponents/NewsDescription';
+import EventNewsCard from './src/components/NewsComponents/EventNewsCard';
+import DetailNewsList from './src/components/NewsDetailComponents/DetailNewsList';
 
-AppRegistry.registerComponent('spotinapp', () => App);
+AppRegistry.registerComponent('spotinapp', () => NewsList);
--- src/components/NewsComponents/EventNewsCard.js
+++ src/components/NewsComponents/EventNewsCard.js
@@ -1,66 +1,100 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, Dimensions } from 'react-native';
+import themes from '../../styleTheme';
+
+const color = themes.base.colors
+//const screenwidth = Dimensions.get('window').width;
+//const screeheight = Dimensions.get('window').height;
+
+const styles = {
+  containerStyle: {
+    marginTop: 10,
+    marginLeft: 8,
+    marginRight: 8,
+    borderRadius: 8,
+    borderColor: '#33CC33',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    position: 'relative',
+    height: 116,
+    width: '100%',
+    backgroundColor: 'white',
+    //shadowOpacity: 1
+  },
+  favouriteContainerStyle: {
+    height: 116,
+    width: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  thumbnailContainerStyle: {
+    borderColor: '#33CC33',
+    justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flexDirection: 'column',
+    height: 116,
+    width: 60,
+  },
+  EventDetailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    height: 116,
+    width: 219
+  },
+  ArrowContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    height: 116,
+    width: 39,
+  }
+};
 
 const EventNewsCard = () => {
 
   return (
     <View style={styles.containerStyle}>
-      <View style={{height: 85, width: 65, justifyContent: 'center', alignItems: 'center'}}>
-        <View style={{height: 35, width: 35, backgroundColor: '#000066'}}>
-          <Image source={{uri:'./NewsIcon/SoccerIcon.png'}} style={{height: 35, width: 35}} />
+      <View style={styles.favouriteContainerStyle}>
+        <View>
+          <Image
+            source={require('./eventicons/favouriteicon.png')}
+            style={{height: 35, width: 35, alignSelf: 'center'}}
+          />
         </View>
       </View>
-        <View style={{height: 85, width: 105, flexDirection: 'column', alignItems: 'center'}}>
-        <View style={{height: 45, width: 45, backgroundColor: '#000066'}}>
-        <Image source={{uri: './NewsIcon/LogoFirstTeam.png'}} style={{height: 45, width: 45}}/>
+        <View style={styles.thumbnailContainerStyle}>
+          <View>
+            <Image
+              source={require('./eventicons/napoliicon.png')}
+              style={{height: 45, width: 45}}
+            />
+          </View>
+          <View>
+            <Image
+             source={require('./eventicons/juveicon.png')}
+             style={{height: 45, width: 45}}
+            />
+          </View>
         </View>
-        <Text>SSC Napoli</Text>
-       </View>
-       <View style={{height: 85, width: 17, alignItems: 'center', justifyContent: 'center'}}>
-         <Text>-</Text>
-       </View>
-       <View style={{height: 85, width: 105, flexDirection: 'column', alignItems: 'center'}}>
-         <View style={{height: 45, width: 45, backgroundColor: '#000066'}}>
-         <Image source={{uri: './NewsIcon/LogoSecondTeam.png'}} style={{height: 45, width: 45}}/>
-         </View>
-        <Text>Juventus</Text>
-       </View>
-      <View style={{height: 85, width: 65, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>13 Gen</Text>
-        <Text>20:43</Text>
-      </View>
+        <View style={styles.EventDetailContainerStyle}>
+          <Text style={{fontSize: 21, marginTop: 5}}>Napoli - Juventus</Text>
+          <Text style={{fontSize: 14, color: color.primary.default}}>3 locali vicino a te</Text>
+          <Text style={{fontSize: 16}}>13 Gen - 15:30</Text>
+        </View>
+        <View style={styles.ArrowContainerStyle}>
+          <Image
+            source={require('./eventicons/arrowicon.png')}
+            style={{height: 20, width: 20, marginRight: 13}}
+          />
+        </View>
     </View>
   );
 }
 
-const styles = {
-  containerStyle: {
-    marginTop: 10,
-    borderColor: '#33CC33',
-    justifyContent: 'flex-start',
-    flexDirection: 'row',
-    position: 'relative',
-    height: 85,
-    width: 359,
-  },
-  imageStyle: {
-    cornerRadius: 5,
-    height: 68,
-    width: 68,
-    justifycontent: 'center',
-    alignItems: 'center',
-    marginLeft: 8,
-    marginRight: 8,
-    marginTop: 11
-  },
-  thumbnailContainerStyle: {
-    borderColor: '#33CC33',
-    backgroundColor: '#000066',
-    justifycontent: 'center',
-    alignItems: 'center',
-    height: 80,
-    width: 80
-  }
-};
+
 
 export default EventNewsCard;
