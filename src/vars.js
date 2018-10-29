a9b4b3a36a697857bd6c0b5278a6c808250dca8a
Dario Guarracino <>
2018-10-29 14:59:27 +0100

    Fix apiUrl

--- src/vars.js
+++ src/vars.js
@@ -1,5 +1,5 @@
 export default {
 
-   // apiUrl: 'https://spotin.herokuapp.com/v1',
-   apiUrl: process.env.NODE_ENV === "production" ? "https://spotin.it/v1" : 'http://localhost:3001/v1'
+    apiUrl: process.env.NODE_ENV === "development" ? "https://spotin-dev.herokuapp.com/v1" : 'https://spotin.herokuapp.com/v1',
+   //apiUrl: process.env.NODE_ENV === "production" ? "https://spotin.it/v1" : 'http://localhost:3001/v1'
 };
