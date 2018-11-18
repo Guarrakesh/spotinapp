69f546e3c0d484c63bc448280e19494e1872e614
alepdb <delbalzo95@gmail.com>
2018-11-18 01:43:43 +0100

    Remote Push Notification (Firebase)

--- android/app/src/main/java/it/spotin/MainApplication.java
+++ android/app/src/main/java/it/spotin/MainApplication.java
@@ -3,7 +3,9 @@ package it.spotin;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.reactnative.push.AppCenterReactNativePushPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
@@ -58,7 +60,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-
+            new RNFirebasePackage(),
+              new RNFirebaseMessagingPackage(),
+              new RNFirebaseNotificationsPackage(),
             new AppCenterReactNativePushPackage(MainApplication.this),
             new ReactNativePushNotificationPackage(),
             new ReactNativeConfigPackage(),
@@ -85,6 +89,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public ReactNativeHost getReactNativeHost() {
+
     return mReactNativeHost;
   }
 
