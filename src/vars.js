a30030ff562401aa5b4e8fa65d4bdc0a2c019440
Dario Guarracino <>
2018-12-10 18:41:50 +0100

    Fix aggiunta offerta salvata nel profilo

--- src/vars.js
+++ src/vars.js
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native';
 import Config from "react-native-config";
 export default {
-  apiUrl: Config.API_URL ? Config.API_URL : process.env.NODE_ENV === "development" ? "https://spotin-dev.herokuapp.com/v1" : "https://spotin.herokuapp.com/v1",
+  //apiUrl: Config.API_URL ? Config.API_URL : process.env.NODE_ENV === "development" ? "https://spotin-dev.herokuapp.com/v1" : "https://spotin.herokuapp.com/v1",
   environment: Config.ENV || "production",
-  //apiUrl: Platform.select({android: 'http://192.168.1.5:3001/v1', ios: 'http://localhost:3001/v1'})
+  apiUrl: Platform.select({android: 'http://192.168.1.5:3001/v1', ios: 'http://localhost:3001/v1'})
 };
